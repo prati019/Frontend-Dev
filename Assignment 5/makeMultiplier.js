11c4752c72c909b9631688e0d28aa9bcc0372c03
Pragati Agrawal <pragatia090@gmail.com>
2025-11-24 04:48:41 +0530

    Added Assignment5

---
+++ Assignment 5/makeMultiplier.js
@@ -0,0 +1,9 @@
+// Higher-order function using closure
+function makeMultiplier(multiplier) {
+    return function (num) {
+        return num * multiplier;
+    };
+}
+
+const triple = makeMultiplier(3);
+console.log(triple(5));
