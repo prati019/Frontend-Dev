481aa028bbc1d9905564209da493b8515ef252e4
Pragati Agrawal <pragatia090@gmail.com>
2025-11-24 03:40:43 +0530

    Added Assignment2

---
+++ Assignment 2/mathUtility.js
@@ -0,0 +1,9 @@
+let x = 16.75;
+
+let rounded = Math.round(x);
+let squareRoot = Math.sqrt(x);
+let cube = Math.pow(x, 3);
+let randomNum = Math.floor(Math.random() * 41) + 10;
+
+console.log(`Number: ${x} Rounded: ${rounded} Square Root: ${squareRoot}
+Cubed Value: ${cube} Random (10–50): ${randomNum}`);
