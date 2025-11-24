4c83e89710ede5cae65344307137e9adfcd4cd40
Pragati Agrawal <pragatia090@gmail.com>
2025-11-24 23:49:33 +0530

    Added Assignment3

--- Assignment 3/debuggingMystery.js
+++ Assignment 3/debuggingMystery.js
@@ -1,8 +1,16 @@
 "use strict";
-// This fails because greeting becomes a global in strict mode.
+
+// Q4: Debugging Mystery – Strict Mode catches undeclared assignment
+
+// This will THROW an error in strict mode:
+// greeting = "Welcome";  // ReferenceError: greeting is not defined
+
 function showMessage() {
-    let greeting = "Welcome";
+    let greeting = "Welcome Guest!";  // Fixed: Proper declaration with let
     console.log(greeting);
 }
 
 showMessage();
+
+
+console.log("Strict mode prevents accidental global variables!");
